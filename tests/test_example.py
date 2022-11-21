a5fcdd8eee1c9c5a59e7728c6c7cb08bb20405bc
ingamingcast <i.a.balyakin@gmail.com>
2022-11-21 23:31:52 +0500

    add stack test

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,3 +6,21 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
 	assert reverse('') == ''
+
+
+def test_stack():
+	stack = []
+	stack.append('one')
+	stack.append('two')
+
+	assert stack.pop() == 'two'
+	assert stack.pop() == 'one'
+
+
+def test_emptiness():
+	stack = []
+	assert not stack
+	stack.append('one')
+	assert bool(stack)
+	stack.pop()
+	assert not stack
